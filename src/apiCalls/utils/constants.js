b775f232084f9f9d971714c6840a22cb44e985c1
catalin.rosu <d9f10ee5c4267b63b9801077a96e4f379057133a>
2022-05-08 10:14:49 +0300

    fix

--- src/apiCalls/utils/constants.js
+++ src/apiCalls/utils/constants.js
@@ -1,2 +1,2 @@
 const development = process.env.NODE_ENV === 'development';
-export const url = development ? 'http://localhost:8080/api/' : 'http://catalinrosu.cloud:8080/';
+export const url = development ? 'http://catalinrosu.cloud:8080/api' : 'http://catalinrosu.cloud:8080/api';
